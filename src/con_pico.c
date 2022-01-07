5ead3bfd2f6b76c5a7c5c3be89ca22c6f65b35dc
Norihiro Kumagai <tendai22plus@gmail.com>
2022-01-07 21:05:33 +0900

    Two files are added

---
+++ src/con_pico.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2016, Takashi TOYOSHIMA <toyoshim@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the authors nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software with out
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUE
+ * NTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#include "con.h"
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/stdio_uart.h"
+
+#define UART_ID uart_default
+#define BAUD_RATE 115200
+
+// We are using pins 0 and 1, but see the GPIO function select table in the
+// datasheet for information on which other pins can be used.
+#define UART_TX_PIN 16
+#define UART_RX_PIN 17
+
+//static int fifo_flag = -1;
+
+static void
+sleep
+(void)
+{
+  sleep_ms(10);   // 10ms wait
+}
+
+void
+con_init
+(void)
+{
+#if 0
+  stdio_init_all();
+  uart_set_baudrate(UART_ID, 115200);
+  // Inital UART
+  gpio_set_function(16, GPIO_FUNC_UART);
+  gpio_set_function(17, GPIO_FUNC_UART);
+#endif
+  stdio_uart_init_full(UART_ID, BAUD_RATE, UART_TX_PIN, UART_RX_PIN);
+  while(uart_is_readable(UART_ID))
+    uart_getc(UART_ID);
+}
+
+void
+con_putchar
+(unsigned char c)
+{
+  while(!uart_is_writable(UART_ID));
+  uart_putc(UART_ID, c);
+}
+
+int
+con_getchar
+(void)
+{
+  if (uart_is_readable(UART_ID)) {
+    return uart_getc(UART_ID);
+  }
+  sleep();
+  return uart_is_readable(UART_ID) ? uart_getc(UART_ID) : -1;
+}
+
+int
+con_peek
+(void)
+{
+  sleep();
+  return uart_is_readable(UART_ID) ? 1 : 0;
+}
