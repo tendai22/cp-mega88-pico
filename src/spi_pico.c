c37cad185dc8a595f018c9705e65cf14137ef43d
Norihiro Kumagai <tendai22plus@gmail.com>
2022-02-03 01:38:12 +0900

    * refine codes in spi_pico.c. * fix unstability in wait_clk routine, now it provides 3.3MHz * adjust timeout values in sdcard_fetch, sdcard_store. * 20ms, 20ms, 250ms.

--- src/spi_pico.c
+++ src/spi_pico.c
@@ -39,8 +39,8 @@
 // pico specific definition
 //
 #define SPI_FILL_CHAR 0xff
+typedef unsigned long reg_t;
 
-#define USE_SOFTSPI
 // SPI Pin Assign
 
 // RX  GP00
@@ -52,9 +52,18 @@
 #define P_CK 2
 #define P_DI 3
 
+#define USE_SOFTSPI
+
 #if defined(USE_SOFTSPI)
 #include "hardware/clocks.h"
 #include "hardware/sync.h"
+
+#else //defined(USE_SOFTSPI)
+#include "hardware/spi.h"
+
+#define SPI_ID spi0
+#endif //defined(USE_SOFTSPI)
+
 //
 //  software spi, using gpio
 //
@@ -67,8 +76,30 @@
 long int
 measured_time;
 
+static unsigned char
+spi_xfer
+(unsigned char out_reg);
 
-static void spi_xfer(unsigned char out_reg, unsigned char *receivep);
+static inline void
+PIN_LOW
+(int pinno)
+{
+  gpio_put(pinno, 0);
+}
+
+static inline void
+PIN_HIGH
+(int pinno)
+{
+  gpio_put(pinno, 1);
+}
+
+static inline reg_t
+PIN_DO
+(void)
+{
+  return gpio_get_all() & (1<<P_DO);
+}
 
 void
 do_spi_init
@@ -78,36 +109,35 @@ do_spi_init
    * CK/DI/CS: out, low
    * DO: in
    */
-  //printf("do_spi_init: start\n");
-  //sleep_ms(100);
+  // gpio spi
   // Port Settings
+#if defined(USE_SOFTSPI)
   gpio_init(P_DO);
   gpio_set_dir(P_DO, GPIO_IN);
 
-  gpio_init(P_DI);
-  gpio_set_dir(P_DI, GPIO_OUT);
-  gpio_put(P_DI, 1);
-
+  gpio_init(P_CS); // CSn
   gpio_init(P_CK);
+  gpio_init(P_DI);
+  
+  PIN_HIGH(P_CS);
+  PIN_LOW(P_CK);
+  PIN_HIGH(P_DI);
+
+  gpio_set_dir(P_DI, GPIO_OUT);
   gpio_set_dir(P_CK, GPIO_OUT);
-  gpio_put(P_CK, 0);
-
-  //unsigned char in, out = 0xff;
-  //for (int i = 0; i < 20; ++i)
-  //  spi_xfer(out, &in);
-
+  gpio_set_dir(P_CS, GPIO_OUT);
+#else //defined(USE_SOFTSPI)
+  // use hardware_spi
+  // Port Settings
+  spi_init(SPI_ID, (unsigned long)10000 * 1000);  // 2Mbit/sec
+  gpio_set_function(P_DO, GPIO_FUNC_SPI); // RX
+  gpio_set_function(P_CK, GPIO_FUNC_SPI); // SCK
+  gpio_set_function(P_DI, GPIO_FUNC_SPI); // TX
   // CSn
   gpio_init(P_CS); // CSn
-  gpio_put(P_CS, 1);
   gpio_set_dir(P_CS, GPIO_OUT);
-}
-
-void
-reset_clk
-(void)
-{
-  cs_deselect();
-  gpio_put(P_CK, 0);
+  gpio_put(P_CS, 1);
+#endif //defined(USE_SOFTSPI)
 }
 
 static inline void
@@ -116,35 +146,64 @@ wait_clk
 {
 // so far, asm nops do not work well. isb seems to help stability
 // of clock pulse width.
-//  asm volatile("isb\n");
 //  asm volatile("nop \n nop \n nop");
 //  asm volatile("nop \n nop \n nop");
-//  asm volatile("nop \n nop \n nop");
-//  asm volatile("nop \n nop \n nop\n nop\n nop \n");
-//  asm volatile("isb\n");
+//  88nop ... 800kHz (No18 okay, No17 no good,)
+//  asm volatile("nop \n nop \n nop\n nop\n nop \n nop \n nop \n nop\n nop\n nop \n");
+//  asm volatile("nop \n nop \n nop\n nop\n nop \n nop \n nop \n nop\n nop\n nop \n");
+#if 1
+  // three pairs, 3.3MHz ... card #18 do not work.
+  //asm volatile("nop\n");
+  //asm volatile("isb\n");
+  // two pairs, 3.3MHz ... card #10 works, maybe all of others will be okay.
+  asm volatile("nop\n");
+  asm volatile("isb\n");
+  // one pair  5.0MHz ... card #10 do not work.
+  asm volatile("nop\n");
+  asm volatile("isb\n");
+#else
   sleep_us(1);    // 1 clock == 2 wait_clks, 2us or 500kHz
+#endif
 }
 
-static void
+static unsigned char
 spi_xfer
-(unsigned char out_reg, unsigned char *receivep)
+(unsigned char out_reg)
 {
+#if defined(USE_SOFTSPI)
   unsigned char in_reg = 0;
   for (int i = 0; i < 8; ++i) {
-    gpio_put(P_CK, 0);
-    gpio_put(P_DI, (out_reg & 0x80) ? 1 : 0);
+    PIN_LOW(P_CK);
+    if (out_reg & 0x80) PIN_HIGH(P_DI);
+    else PIN_LOW(P_DI);
     wait_clk();
-    if (gpio_get(P_DO)) in_reg |= 1;
-    gpio_put(P_CK, 1);
+    if (PIN_DO()) in_reg |= 1;
+    PIN_HIGH(P_CK);
     wait_clk();
     if (i == 7) break;
     out_reg <<= 1;
     in_reg <<= 1;
   }
-  gpio_put(P_CK, 0);
+  PIN_LOW(P_CK);
   //gpio_put(P_DI, 1);
-  if (receivep)
-    *receivep = in_reg;
+  return in_reg;
+#else
+  unsigned char in_reg;
+  spi_write_read_blocking(SPI_ID, &out_reg, &in_reg, 1);
+  return in_reg;
+#endif //defined(USE_SOFTSPI)
+}
+
+//
+// The following codes are machine/cpu independent codes
+//
+
+void
+reset_clk
+(void)
+{
+  cs_deselect();
+  PIN_LOW(P_CK);
 }
 
 int
@@ -152,10 +211,20 @@ sd_wait_resp
 (unsigned char value, long int counter, int bitnum)
 {
   unsigned char c;
+  reg_t mask = value ? (((reg_t)(-1)) & M_DO) : 0;
+  reg_t x;
   long int ncounter = counter;
   long int nstart = ncounter;
-  if (bitnum == 8) {
-    while (ncounter > 0 && (c = sd_in()) == value) {
+#if defined(USE_SOFTSPI)
+  if (bitnum != 8) {
+    ncounter *= 8;  // the following loop period is counter * 8clocks.
+    PIN_HIGH(P_DI);  // out 1 on DI
+    while (ncounter > 0) {
+      PIN_LOW(P_CK);
+      wait_clk();
+      if (PIN_DO() != mask) break;
+      PIN_HIGH(P_CK);
+      wait_clk();
       ncounter--;
     }
     if (ncounter <= 0) {
@@ -164,134 +233,48 @@ sd_wait_resp
 #if defined(SDCARD_DEBUG)
     measured_time = nstart - ncounter;
 #endif
-    return c;
+    return spi_xfer(SPI_FILL_CHAR);
   }
-  ncounter *= 8;  // the following loop period is counter * 8clocks.
-  gpio_put(P_DI, 1);  // out 1 on DI
-  while (ncounter > 0) {
-    gpio_put(P_CK, 0);
-    wait_clk();
-    if (!gpio_get(P_DO) != !value) break;
-    gpio_put(P_CK, 1);
-    wait_clk();
+#endif //defined(USE_SOFTSPI)
+  while (ncounter > 0 && (c = sd_in()) == value)
     ncounter--;
-  }
-  if (ncounter <= 0) {
-    return -1;
-  }
+  if (ncounter <= 0) return -1;
 #if defined(SDCARD_DEBUG)
   measured_time = nstart - ncounter;
 #endif
-  spi_xfer(0xff, &c);
   return c;
-
 }
 
 unsigned long
 sd_in
 (void)
 {
-  uint8_t value = SPI_FILL_CHAR;
-  uint8_t received;
-  spi_xfer(value, &received);
-  return received;
+  return spi_xfer(SPI_FILL_CHAR);
 }
 
 void
 sd_out
 (const unsigned char c)
 {
-  uint8_t dummy;
-  spi_xfer(c, &dummy);
+  spi_xfer(c);
 }
 
 void cs_select()
 {
-//  asm volatile("nop \n nop \n nop");
-//  sd_out(0xff);
-//  asm volatile("nop \n nop \n nop");
-  gpio_put(P_CS, 0);  // Active low
+  PIN_LOW(P_CS);  // Active low
 }
 
 void cs_deselect()
 {
-//  asm volatile("nop \n nop \n nop");
-  gpio_put(P_CS, 1);  // Active low
-  //sd_out(0xff);
-//  asm volatile("nop \n nop \n nop");
+  PIN_HIGH(P_CS);  // Active low
 }
 
-#else //defined(USE_SOFTSPI)
-#include "hardware/spi.h"
 //
 // use hardware_spi in pico-sdk
 //
 #define SPI_ID spi0
 
-void
-do_spi_init
-(void)
-{
-  /*
-   * CK/DI/CS: out, low
-   * DO: in
-   */
-  // Port Settings
-  spi_init(spi0, 2000 * 1000);  // 2Mbit/sec
-  gpio_set_function(P_DO, GPIO_FUNC_SPI); // RX
-  gpio_set_function(P_CK, GPIO_FUNC_SPI); // SCK
-  gpio_set_function(P_DI, GPIO_FUNC_SPI); // TX
-  // CSn
-  gpio_init(P_CS); // CSn
-  gpio_set_dir(P_CS, GPIO_OUT);
-  gpio_put(P_CS, 1);
-}
-
-void
-reset_clk
-(void)
-{
-  cs_deselect();
-  gpio_put(P_CK, 0);
-}
-
-int
-sd_wait_resp
-(unsigned char value, long int counter, int bitnum)
-{
-  unsigned char c;
-  long int ncounter = counter;
-  long int nstart = ncounter;
-  while (ncounter > 0 && (c = sd_in()) == value) {
-    ncounter--;
-  }
-  if (ncounter <= 0) {
-    return -1;
-  }
-#if defined(SDCARD_DEBUG)
-    measured_time = nstart - ncounter;
-#endif
-  return c;
-}
-
-unsigned long
-sd_in
-(void)
-{
-  uint8_t value = SPI_FILL_CHAR;
-  uint8_t received;
-  spi_write_read_blocking(SPI_ID, &value, &received, 1);
-  return received;
-}
-
-void
-sd_out
-(const unsigned char c)
-{
-  uint8_t dummy;
-  spi_write_read_blocking(SPI_ID, &c, &dummy, 1);
-}
-
+#if 0
 void cs_select()
 {
   asm volatile("nop \n nop \n nop");
@@ -307,5 +290,4 @@ void cs_deselect()
   sd_out(0xff);
   asm volatile("nop \n nop \n nop");
 }
-
 #endif //define(USE_SOFT_SPI)
