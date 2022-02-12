350ba8fa0bd7eff56ea0642f3636bfee3f1d6640
Norihiro Kumagai <tendai22plus@gmail.com>
2022-02-13 02:47:04 +0900

    Add delete_files.sh

---
+++ src/delete_files.sh
@@ -0,0 +1,23 @@
+xargs rm <<"EOF"
+       con_pico.c
+       con_posix.c
+       con_uboot.c
+       con_uefi.c
+       eeprom_pico.c
+       eeprom_posix.c
+       eeprom_uboot.c
+       eeprom_uefi.c
+       io_pico.c
+       led_pico.c
+       platform_nacl.h
+       platform_pico.c
+       platform_posix.c
+       platform_uboot.c
+       platform_uefi.c
+       platform_nacl.c
+       sdcard_picoflash.c
+       sdcard_posix.c
+       sdcard_softpico.c
+       sdcard_uboot.c
+       sdcard_uefi.c
+       sram_pico.c
